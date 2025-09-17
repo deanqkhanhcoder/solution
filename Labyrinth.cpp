131b19bc71ed2e85bf36a2cd4f01d0e7e5a04a68
deanqkhanhcoder <pquockhanh09102010@gmail.com>
2025-09-17 17:53:53 +0700

    v5

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -23,7 +23,7 @@ int main(){
     }
     queue<pair<int,int>> q;
     vector<vector<bool>> visited(n, vector<bool>(m, false));
-    vector<vector<pair<int, int>>> parent(n, vector<pair<int, int>>(m));
+    vector<vector<pair<int, int>>> pt(n, vector<pair<int, int>>(m));
     vector<vector<char>> move(n, vector<char>(m));
     q.push(apos);
     visited[apos.first][apos.second] = true;
@@ -38,7 +38,7 @@ int main(){
             int ny = y + dy[k];
             if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && grid[nx][ny] != '#') {
                 visited[nx][ny] = true;
-                parent[nx][ny] = {x, y};
+                pt[nx][ny] = {x, y};
                 move[nx][ny] = dmove[k];
                 q.push({nx, ny});
             }
@@ -53,7 +53,7 @@ int main(){
     while (bpos != apos) {
         auto [x, y] = bpos;
         path.push_back(move[x][y]);
-        bpos = parent[x][y];
+        bpos = pt[x][y];
     }
     reverse(path.begin(), path.end());
     cout << "YES\n";
