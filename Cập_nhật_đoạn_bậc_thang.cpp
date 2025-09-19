297d0c23038538edff00ea1d2baf7512d88f1788
deanqkhanhcoder <pquockhanh09102010@gmail.com>
2025-09-19 12:13:49 +0700

    k

--- Cập_nhật_đoạn_bậc_thang.cpp
+++ Cập_nhật_đoạn_bậc_thang.cpp
@@ -10,20 +10,44 @@ int main() {
     cin.tie(nullptr);
     int n, q; cin >> n >> q;
     vector<ll> d(n + 2, 0);
+    vector<int> g(n + 2, 0);
     while (q--) {
         int l, r;
         cin >> l >> r;
         d[l] += 1;
-        d[r + 1] -= (r - l + 2);
+        d[r + 1] -= 1;
+        g[r + 1] -= (r - l + 1);
     }
-    for (int i = 1; i <= n; i++) {
-        d[i] += d[i - 1];
-    }
-    for (int i = 1; i <= n; i++) {
-        d[i] += d[i - 1];
-    }
+    /*
+    cout << "d : ";
     for (int i = 1; i <= n; i++) {
         cout << d[i] << " ";
+    }
+    cout << "\n";
+    cout << "g : ";
+    for (int i = 1; i <= n; i++) {
+        cout << g[i] << " ";
+    }
+    */
+    for (int i = 1; i <= n; i++) {
+        d[i] += d[i - 1];
+        g[i] += g[i - 1];
+    }
+    /*
+    cout << "\n";
+    cout << "d : ";
+    for (int i = 1; i <= n; i++) {
+        cout << d[i] << " ";
+    }
+    cout << "\n";
+    cout << "g : ";
+    for (int i = 1; i <= n; i++) {
+        cout << g[i] << " ";
+    }
+    */
+    for (int i = 1; i <= n; i++) {
+        d[i] += d[i - 1];
+        cout << d[i] + g[i] << " ";
     }
     return 0;
 }
