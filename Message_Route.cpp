416f3a61fd7150bb6fc540fb0c37dbff803e794a
deanqkhanhcoder <pquockhanh09102010@gmail.com>
2025-09-19 17:45:59 +0700

    graph

---
+++ Message_Route.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m; cin >> n >> m;
+    vector<vector<int>> pos(n + 1);
+    for (int i = 0; i < m; ++i){
+        int a, b; cin >> a >> b;
+        pos[a].push_back(b);
+        pos[b].push_back(a);
+    }
+    vector<int> ans(n + 1, -1);
+    ans[1] = 0;
+    vector<int> parent(n + 1, -1);
+    queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (int &v : pos[u]) {
+            if (ans[v] == -1){
+                ans[v] = ans[u] + 1;
+                parent[v] = u;
+                q.push(v);
+            }
+            }
+        }
+    if (ans[n] == -1){
+        cout << "IMPOSSIBLE";
+        return 0;
+    }
+    cout << ans[n] + 1 << "\n";
+    string s = to_string(n);
+    while (n > 1){
+        s = to_string(parent[n]) + " " + s;
+        n = parent[n];
+    }
+    cout << s;
+    return 0;
+}
