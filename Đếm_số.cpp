408596ec9e92d478ffa6bbdc652f0decaaa4ea17
deanqkhanhcoder <pquockhanh09102010@gmail.com>
2025-10-12 21:33:34 +0700

    ok

---
+++ Đếm_số.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    co
+
+    
+}
