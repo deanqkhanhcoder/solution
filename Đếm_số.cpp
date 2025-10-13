234bb3d45d27f0fd1200e9a1bce5166600f55513
pqkhanh <pquockhanh09102010@gmail.com>
2025-10-13 13:14:13 +0700

    ok

--- Đếm_số.cpp
+++ Đếm_số.cpp
@@ -4,10 +4,26 @@
 #include <iostream>
 using namespace std;
 using ll = long long;
-
+using i128 = __int128_t;
+const int MOD = (int)1e9 + 7;
+ll binpow(ll a, ll b){
+    ll res = 1;
+    a %= MOD;
+    while (b > 0){
+        if (b & 1) res = ((i128)res * a) % MOD;
+        a = ((i128)a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
 int main(){
     ios::sync_with_stdio(false);
-    co
-
-    
+    cin.tie(nullptr);
+    ll n; cin >> n;
+    ll cnt0 = n / 3;
+    ll cnt1 = (n + 1) / 3;
+    ll cnt2 = (n + 2) / 3;
+    ll res = (i128)binpow(3, cnt1) * binpow(3, cnt2) % MOD * binpow(4, cnt0) % MOD;
+    cout << res % MOD;
+    return 0;
 }
