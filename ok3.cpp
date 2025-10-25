8bd4084d2ae3d5362e58e2fc25fb91364543f036
pqkhanh <pquockhanh09102010@gmail.com>
2025-10-25 08:25:12 +0700

    sure

--- ok3.cpp
+++ ok3.cpp
@@ -6,21 +6,21 @@
 #define endl "\n"
 using namespace std;
 
-const int N = (int)1e7;
+const int limit = (int)1e7;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    bitset<N + 1> ip;
+    bitset<limit + 1> ip;
     ip.set();
     ip[0] = ip[1] = 0;
-    for (int i = 4; i <= N; i += 2) ip[i] = 0;
-    for (int i = 3; 1LL * i * i <= N; i += 2)
+    for (int i = 4; i <= limit; i += 2) ip[i] = 0;
+    for (int i = 3; 1LL * i * i <= limit; i += 2)
         if (ip[i])  
-            for (int j = i * i; j <= N; j += 2 * i)
+            for (int j = i * i; j <= limit; j += 2 * i)
                 ip[j] = 0;
-    static int ds[N + 1];
-    for (int i = 1; i <= N; ++i)
+    static int ds[limit + 1];
+    for (int i = 1; i <= limit; ++i)
         ds[i] = ds[i / 10] + (i % 10);
     int l, r; cin >> l >> r;
     for (int i = l; i <= r; ++i)
