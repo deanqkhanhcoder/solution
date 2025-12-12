4729ebb16c79328fa7ffc0487278da9643a470a3
pqkhanh <pquockhanh09102010@gmail.com>
2025-12-12 19:39:12 +0700

    dạ vâng :(

--- Subordinates.cpp
+++ Subordinates.cpp
@@ -4,35 +4,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-vector<vector<int>> tree;
-vector<int> subordinates;
-
-int dfs(int node) {
-    int count = 0;
-    for (int child : tree[node]) {
-        count += 1 + dfs(child);
+vector<vector<int>> g;
+vector<int> ans;
+int n;
+int dfs(int u) {
+    for (int v : g[u]) {
+        ans[u] += 1;
+        dfs(v);
     }
-    subordinates[node] = count;
-    return count;
+    return ans[u];
 }
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
     cin >> n;
-    tree.assign(n + 1, vector<int>());
-    subordinates.assign(n + 1, 0);
-
-    for (int i = 2; i <= n; ++i) {
-        int boss;
-        cin >> boss;
-        tree[boss].push_back(i);
+    g.assign(n+1, {});
+    ans.assign(n+1, 0);
+    for (int i = 2; i <= n; i++) {
+        int p;
+        cin >> p;
+        g[p].push_back(i);
     }
     dfs(1);
-    for (int i = 1; i <= n; ++i) {
-        cout << subordinates[i] << " ";
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
     }
     return 0;
 }
